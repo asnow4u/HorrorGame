e4fa2dd8c3d81e0ffd976a1cc0156abd53ddff8a
Andrew <asnow4u@gmail.com>
2022-05-04 21:55:09 -0500

    Updates

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    public float walkSpeed;
+    public float runSpeed;
 
-    public float speed = 12f;
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 dir = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            transform.Translate(dir * runSpeed * Time.deltaTime, Space.World);
+        }
+
+        else
+        {
+            transform.Translate(dir * walkSpeed * Time.deltaTime, Space.World);
+        }
+
     }
 }
