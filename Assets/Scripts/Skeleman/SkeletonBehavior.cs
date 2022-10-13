782d7daecafc552bc31a55fa1754c9f2868c270f
Andrew <asnow4u@gmail.com>
2022-10-12 20:02:29 -0500

    Skeleboy be watchin

--- Assets/Scripts/Skeleman/SkeletonBehavior.cs
+++ Assets/Scripts/Skeleman/SkeletonBehavior.cs
@@ -14,7 +14,12 @@ public partial class SkeletonBehavior : MonoBehaviour
 
     public static SkeletonBehavior instance;
 
+    [Header("Skeleton Properties")]
     [SerializeField] private GameObject skeleton;
+    [SerializeField] private Transform skeletonHead;
+
+    [SerializeField] private float sightAngle;
+    [SerializeField] private float sightRange;
 
     //State
     public enum State {dormant, observe, wander, hunt, chase};
@@ -132,44 +137,95 @@ public partial class SkeletonBehavior : MonoBehaviour
 
     private void ProgressCheck()
     {
+
+        //if (state == State.observe || state == State.wander || state == State.hunt)
+        {
+            if (CheckForPlayer())
+            {
+                state = State.chase;
+            }
+        }
+
+
+
+        //TODO: change to be time based
+
         //% of gathered key and or time based
-        int totalKeys = KeyController.instance.HeldGoldKeys;
-        totalKeys += KeyController.instance.HeldSilverKeys;
-        totalKeys += KeyController.instance.UsedGoldKeys;
-        totalKeys += KeyController.instance.UsedSilverKeys;
+        //int totalKeys = KeyController.instance.HeldGoldKeys;
+        //totalKeys += KeyController.instance.HeldSilverKeys;
+        //totalKeys += KeyController.instance.UsedGoldKeys;
+        //totalKeys += KeyController.instance.UsedSilverKeys;
 
-        keyPercent = (float)totalKeys / (float)(KeyController.instance.StartGoldKeys + KeyController.instance.StartSilverKeys);
+        //keyPercent = (float)totalKeys / (float)(KeyController.instance.StartGoldKeys + KeyController.instance.StartSilverKeys);
       
-        //dormant to observe 20%
-        if (state == State.dormant)
-        {
-            if (keyPercent > .2f)
-            {
-                state = State.observe;
-                Debug.Log("Skeleton: State Change to Observant");
-            }
-        }
+        ////dormant to observe 20%
+        //if (state == State.dormant)
+        //{
+        //    if (keyPercent > .2f)
+        //    {
+        //        state = State.observe;
+        //        Debug.Log("Skeleton: State Change to Observant");
+        //    }
+        //}
 
-        //observe to wander 40%
-        else if (state == State.observe)
-        {
-            if (keyPercent > .4f)
-            {
-                state = State.wander;
-                Debug.Log("Skeleton: State Change to Wander");
-                wanderState = WanderState.startWander;
-                Debug.Log("Skeleton: WanderState Change to StartWander");
-                skeleton.GetComponent<SkeletonMovement>().enabled = true;
-                skeleton.GetComponent<NavMeshAgent>().enabled = true;
-            }
-        }
+        ////observe to wander 40%
+        //else if (state == State.observe)
+        //{
+        //    if (keyPercent > .4f)
+        //    {
+        //        state = State.wander;
+        //        Debug.Log("Skeleton: State Change to Wander");
+        //        wanderState = WanderState.startWander;
+        //        Debug.Log("Skeleton: WanderState Change to StartWander");
+        //        skeleton.GetComponent<SkeletonMovement>().enabled = true;
+        //        skeleton.GetComponent<NavMeshAgent>().enabled = true;
+        //    }
+        //}
 
         //Random hunts can only occure when wandering
         if (state == State.wander)
         {
             CheckHuntTimer();
         }
+
+        
     }
+
+
+    public bool CheckForPlayer()
+    {
+        Vector3 rayVector = PlayerController.instance.MainCamera.transform.position - skeletonHead.position;
+
+        Debug.DrawLine(skeletonHead.position, PlayerController.instance.MainCamera.transform.position);
+
+        //Check for angle
+        float angle = Vector3.Angle(-skeletonHead.transform.up, rayVector);
+        
+        if (angle <= sightAngle)
+        {
+
+            Ray ray = new Ray(skeletonHead.position, rayVector);
+            Debug.DrawRay(skeletonHead.position, rayVector);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, sightRange, LayerMask.NameToLayer("Room")))
+            {
+                Debug.Log(hit.transform.name);
+
+                if (hit.transform.tag == "Player")
+                {
+
+                    Debug.Log("Player seen");
+
+
+                }
+            }
+        }
+
+        return false;
+    }
+
 
 
     private IEnumerator Wait(float time)
--- Assets/Scripts/Skeleman/SkeletonBehavior_Chase.cs
+++ Assets/Scripts/Skeleman/SkeletonBehavior_Chase.cs
@@ -8,11 +8,11 @@ public partial class SkeletonBehavior : MonoBehaviour
 
     /*Requirements
      
-     * Chase: Sekelman is chasing the player
+     * Chase: Skeleman is chasing the player
      * When the player is seen the skeleman will chase them. (Navmesh just following them)
      * If line of sight is broken, they will proceed to the last know location and look in the direction the player was looking at that time. 
      *      If they see the player again they will continue pursuit.
-     *      If not, they will search the room for a bit, then will destroy on of the hidable locations. If only one remains the player is caught. If more than one exists the other hiding place will be destroyed
+     *      If not, they will search the room for a bit, then will destroy one of the hidable locations. If only one remains the player is caught. If more than one exists the other hiding place will be destroyed
      *          We will need to determine that the direction vector points to a room
      * If the skeleman sees the player run into a hiding spot, they will be caught 
     */
@@ -21,7 +21,13 @@ public partial class SkeletonBehavior : MonoBehaviour
 
     private void UpdateChase()
     {
-       
+        //TODO:
+        //Update navmesh to follow of player
+        //if line of sight broken go to the last point and guess (look in nearby room)
+
+        //Alway break an object if person is not found
+
+        Debug.Log("Chase Player");
      
     } 
 }
